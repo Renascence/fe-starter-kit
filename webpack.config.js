247bf300f62702c2534637a8b764d5f87cd0de13
logging <geniusglory@outlook.com>
2019-03-26 00:00:19 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/build",
+    filename: '[name].js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    port: "7000",
+    // contentBase: __dirname + "/build",
+    inline: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          ['import', { libraryName: "antd", style: true }],
+        ]
+      },
+      // query: {
+      // }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.less$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "less-loader"
+      }]
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ]
+};
